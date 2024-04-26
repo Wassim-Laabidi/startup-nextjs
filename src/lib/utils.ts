00f5e84d95099f2fe9363d60ad52ed99d6426a12
Wassim Laabidi <laabidi.wassim250@gmail.com>
2024-04-26 22:12:20 +0100

    i finished the basic profile page but it still take data from given userId

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getJoinedDate = (date: Date): string => {
+  // Extract the month and year from the Date object
+  const month = date.toLocaleString("default", { month: "long" });
+  const year = date.getFullYear();
+  // Create the joined date string (e.g., "September 2023")
+  const joinedDate = `${month} ${year}`;
+
+  return joinedDate;
+};
